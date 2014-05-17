64684b3d92d40de3d55dbd333fd847fe467dedce
Jonatan Schumacher <jonatan@jschumacher.com>
2014-05-17 14:47:58 -0400

    started with scene

--- GHva3c/GHva3c/va3c_Scene.cs
+++ GHva3c/GHva3c/va3c_Scene.cs
@@ -23,9 +23,10 @@ namespace GHva3c
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddTextParameter("Geometry", "G", "va3c geometry", GH_ParamAccess.item);
-            pManager.AddTextParameter("[Lights]", "[L]", "va3c light sources", GH_ParamAccess.item);
-            pManager.AddTextParameter("[Cameras]", "[C]", "va3c cameras", GH_ParamAccess.item);
+            pManager.AddTextParameter("Geometry", "G", "va3c geometry", GH_ParamAccess.list);
+            pManager.AddTextParameter("Materials", "M", "va3c materials", GH_ParamAccess.list);
+            pManager.AddTextParameter("[Lights]", "[L]", "va3c light sources", GH_ParamAccess.list);
+            pManager.AddTextParameter("[Cameras]", "[C]", "va3c cameras", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -33,6 +34,7 @@ namespace GHva3c
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddTextParameter("Message", "Out", "Message", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -41,6 +43,21 @@ namespace GHva3c
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+
+            //variables
+
+             List<string> inGeometry = new List<string>();
+             List<string> inMaterials = new List<string>();
+
+            //get user inputs
+            if (!DA.GetData(0, ref inGeometry)) return;
+            if (!DA.GetData(1, ref inMaterials)) return;
+
+            //compile geometry + materials into one object with metadata etc.
+            //https://raw.githubusercontent.com/mrdoob/three.js/master/examples/obj/blenderscene/scene.js
+
+
+
         }
 
         /// <summary>
