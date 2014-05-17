2ba6a41e135f5208980ea4ddf16346d83b163582
Benjamin Howes <howesben@gmail.com>
2014-05-17 11:09:45 -0400

    Added Visual Studio 2010 solution
    And a blank GH component, assembly info classes.

---
+++ GHva3c/GHva3c/GHva3cInfo.cs
@@ -0,0 +1,18 @@
+﻿using Grasshopper.Kernel;
+
+namespace GHva3c
+{
+    public class GHva3cInfo : GH_AssemblyInfo
+    {
+        public override string AssemblyName
+        {
+            get
+            {
+                return "GHva3c";
+            }
+        }
+
+        //Override here any more methods you see fit.
+        //Start typing public override..., select a property and push Enter.
+    }
+}
