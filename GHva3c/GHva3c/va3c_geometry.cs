054512678939ba6a4b9513738b8414c44f3f7b84
Benjamin Howes <howesben@gmail.com>
2014-05-17 13:49:38 -0400

    1st pass at geometry component
    outputs json.  need to match the json format that describes objects...

--- GHva3c/GHva3c/va3c_geometry.cs
+++ GHva3c/GHva3c/va3c_geometry.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Dynamic;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Timers;
 
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
+
+using Newtonsoft.Json;
 
 namespace GHva3c
 {
@@ -26,8 +33,10 @@ namespace GHva3c
             pManager.AddMeshParameter("Meshes", "M", "Triangulated Meshes", GH_ParamAccess.item);
             pManager.AddTextParameter("Materials", "[Mat]", "Materials", GH_ParamAccess.item);
             pManager[1].Optional = true;
-            pManager.AddTextParameter("Attribute Names", "aN", "Attribute Names", GH_ParamAccess.list);
-            pManager.AddNumberParameter("Attribute Values", "aV", "Attribute Values", GH_ParamAccess.list);
+            pManager.AddTextParameter("Attribute Names", "[aN]", "Attribute Names", GH_ParamAccess.list);
+            pManager[2].Optional = true;
+            pManager.AddNumberParameter("Attribute Values", "[aV]", "Attribute Values", GH_ParamAccess.list);
+            pManager[3].Optional = true;
         }
 
         /// <summary>
@@ -44,6 +53,57 @@ namespace GHva3c
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            //local varaibles
+            GH_Mesh mesh = null;
+
+
+            //catch inputs and populate local variables
+            if (!DA.GetData(0, ref mesh))
+            {
+                return;
+            }
+            if (mesh == null)
+            {
+                return;
+            }
+
+            //create json from mesh
+            string outJSON = geoJSON(mesh.Value);
+
+            DA.SetData(0, outJSON);
+            
+        }
+
+        private string geoJSON(Mesh mesh)
+        {
+            //create a dynamic object to populate
+            dynamic jason = new ExpandoObject();
+            jason.vertices = new object[mesh.Vertices.Count*3];
+            jason.faces = new object[mesh.Faces.Count*3];
+
+            //populate vertices
+            int counter = 0;
+            int i = 0;
+            foreach (var v in mesh.Vertices)
+            {
+                jason.vertices[counter++] = mesh.Vertices[i].X;
+                jason.vertices[counter++] = mesh.Vertices[i].Y;
+                jason.vertices[counter++] = mesh.Vertices[i].Z;
+                i++;
+            }
+
+            //populate faces
+            counter = 0;
+            i = 0;
+            foreach (var f in mesh.Faces)
+            {
+                jason.faces[counter++] = mesh.Faces[i].A;
+                jason.faces[counter++] = mesh.Faces[i].B;
+                jason.faces[counter++] = mesh.Faces[i].C;
+                i++;
+            }
+
+            return JsonConvert.SerializeObject(jason);
         }
 
         /// <summary>
