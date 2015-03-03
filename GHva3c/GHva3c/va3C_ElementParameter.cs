7f9fd08196331f07b4ae405eda00bdc5ba13fba8
Ana <anagpuyol@gmail.com>
2015-03-03 15:00:33 -0500

    Material and Element Parameters registered

---
+++ GHva3c/GHva3c/va3C_ElementParameter.cs
@@ -0,0 +1,50 @@
+﻿/*  HEADER
+ * 
+ *  Custom GH Parameter : Element
+ *  This register the custom object as a GH parameter
+ *  //http://www.grasshopper3d.com/forum/topics/custom-data-and-parameter-no-implicit-reference-conversion
+ * 
+ *  03/03/15 
+ *  Ana Garcia Puyol
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace GHva3c
+{
+    class MaterialParameter:GH_Goo<Material>
+    {
+        public override IGH_Goo Duplicate()
+        {
+            MaterialParameter element = new MaterialParameter();
+            element.Value = Value;
+            return element;
+        }
+
+        public override string TypeDescription
+        {
+            get { return "Material: A high level vA3C class to inherit from"; }
+        }
+
+        public override string TypeName
+        {
+            get { return "Material"; }
+        }
+
+        public override bool IsValid
+        {
+            get { return Value.ID != null; }
+        }
+
+        public override string ToString()
+        {
+            return Value.ID;
+        }
+    }
+}
---
+++ GHva3c/GHva3c/va3C_MaterialParameter.cs
@@ -0,0 +1,50 @@
+﻿/*  HEADER
+ * 
+ *  Custom GH Parameter : Element
+ *  This register the custom object as a GH parameter
+ *  //http://www.grasshopper3d.com/forum/topics/custom-data-and-parameter-no-implicit-reference-conversion
+ * 
+ *  03/03/15 
+ *  Ana Garcia Puyol
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace GHva3c
+{
+    class ElementParameter:GH_Goo<Element>
+    {
+        public override IGH_Goo Duplicate()
+        {
+            ElementParameter element = new ElementParameter();
+            element.Value = Value;
+            return element;
+        }
+
+        public override string TypeDescription
+        {
+            get { return "Element: A high level vA3C class to inherit from"; }
+        }
+
+        public override string TypeName
+        {
+             get { return "Element"; }
+        }
+
+        public override bool IsValid
+        {
+            get { return Value.ID != null; }
+        }
+
+        public override string ToString()
+        {
+            return Value.ID;
+        }
+    }
+}
