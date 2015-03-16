f186d0534534cea4be5d813fa32ebe3d276b6a41
Ana <anagpuyol@gmail.com>
2015-03-16 17:22:18 -0400

    Layer parameter and class created

---
+++ GHva3c/GHva3c/va3C_LayerParameter.cs
@@ -0,0 +1,50 @@
+﻿/*  HEADER
+ * 
+ *  Custom GH Parameter : Element
+ *  This register the custom object as a GH parameter
+ *  //http://www.grasshopper3d.com/forum/topics/custom-data-and-parameter-no-implicit-reference-conversion
+ * 
+ *  03/03/15 
+ *  Ana Garcia Puyol
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace GHva3c
+{
+    class LayerParameter:GH_Goo<Layer>
+    {
+        public override IGH_Goo Duplicate()
+        {
+            LayerParameter layer = new LayerParameter();
+            layer.Value = Value;
+            return layer;
+        }
+
+        public override string TypeDescription
+        {
+            get { return "Layer: A high level vA3C class to inherit from"; }
+        }
+
+        public override string TypeName
+        {
+            get { return "Layer"; }
+        }
+
+        public override bool IsValid
+        {
+            get { return Value.ID != null; }
+        }
+
+        public override string ToString()
+        {
+            return Value.ID;
+        }
+    }
+}
---
+++ GHva3c/GHva3c/va3c_Layer.cs
@@ -0,0 +1,75 @@
+﻿/*  HEADER
+ * 
+ * ELEMENT CLASS  
+ *  A high level class to inherit from.  Provides fields that all model elements must have.
+ *  
+ *  3/3/15
+ *  
+ * Ana Garcia Puyol
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using GHva3c.Properties;
+
+
+using Newtonsoft.Json;
+
+namespace GHva3c
+{
+
+    public class Layer
+    {
+        //ATTRIBUTES
+
+        private string myID;
+
+       // public va3cElementType Type { get; set; }
+
+        public string Name { get; set; }
+
+        //PROPERTIES
+        //[DataMember]
+        public string ID
+        {
+            get { return myID; }
+            set
+            {
+                try
+                {
+                    //test for the empty string
+                    if (value == "")
+                    {
+                        throw new ArgumentException("The input string cannot be empty");
+                    }
+
+                    myID = value;
+                }
+
+                catch (Exception e) //should catch the null case
+                {
+                    throw e;
+                }
+            }
+        }
+
+        public Layer() { }
+        public Layer(string name)
+        {
+            Name = name;
+        }
+
+
+    }
+
+   
+}
